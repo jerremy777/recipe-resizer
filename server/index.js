ec7353dc85870079b02702902c641458f39aac4c
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-17 19:35:58 -0700

    Basic server working, not serving static files yet

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+app.use(express.static(__dirname + '../dist'));
+
+app.get('/', (req, res) => {
+  console.log('request from:', req.url);
+  res.send('ok');
+});
+
 
 app.listen(port);
 console.log('App listening on port:', port);
