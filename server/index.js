9870cc00e7988caa3937cd6c0eb0371aa8d322db
Jerremy Stroman <jerremystroman@gmail.com>
2022-04-17 19:47:18 -0700

    npm scripts. working on static file server

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ const port = process.env.PORT;
 
 app.use(express.static(__dirname + '../dist'));
 
-app.get('/', (req, res) => {
-  console.log('request from:', req.url);
-  res.send('ok');
-});
+// app.get('/', (req, res) => {
+//   console.log('request from:', req.url);
+//   res.send('ok');
+// });
 
 
 app.listen(port);
 console.log('App listening on port:', port);
+console.log(__dirname + '../dist');
