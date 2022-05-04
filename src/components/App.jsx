409e59a108f7d56c0ee11325f22771b8a30477f6
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-03 18:50:35 -0700

    Further destructuring components for maintainance of state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,30 +9,30 @@ function App(props) {
   const [ingredients, setIngredients] = React.useState([...Array(recipeLength)]);
   const [scaledIngredients, setScaledIngredients] = React.useState([...Array(recipeLength)]);
 
-  const handleRecipeChange = (e) => {
-    const list = ingredients;
-    list[e.target.id] = Number(e.target.value);
-    console.log('ingredients:', ingredients);
-    setIngredients(list);
-    setScaledIngredients(list);
-  };
+  // const handleRecipeChange = (e) => {
+  //   const list = ingredients;
+  //   list[e.target.id] = Number(e.target.value);
+  //   console.log('ingredients:', ingredients);
+  //   setIngredients(list);
+  //   setScaledIngredients(list);
+  // };
 
-  const handleScaleChange = (e) => {
-    setScaledIngredients((previous) => ({ ...previous, [e.target.id]: e.target.value * 1 }));
-  };
+  // const handleScaleChange = (e) => {
+  //   setScaledIngredients((previous) => ({ ...previous, [e.target.id]: e.target.value * 1 }));
+  // };
 
-  const ingredientElement = (index) => (
-    <li key={index}>
-      <label htmlFor={index}>Ingredient:</label>
-      <input type="text" id={index} onChange={(e) => handleRecipeChange(e)} />
-    </li>
-  );
+  // const ingredientElement = (index) => (
+  //   <li key={index}>
+  //     <label htmlFor={index}>Ingredient:</label>
+  //     <input type="text" id={index} onChange={(e) => handleRecipeChange(e)} />
+  //   </li>
+  // );
 
-  const handleButtonClick = (e) => {
-    const newFactor = factor * e.target.value;
-    setFactor(newFactor);
-    setScaledIngredients((previousIngredients) => previousIngredients.map((e) => e * newFactor));
-  };
+  // const handleButtonClick = (e) => {
+  //   const newFactor = factor * e.target.value;
+  //   setFactor(newFactor);
+  //   setScaledIngredients((previousIngredients) => previousIngredients.map((e) => e * newFactor));
+  // };
 
   return (
     <div id="main-app">
@@ -50,7 +50,6 @@ function App(props) {
       <div id="resizer-headers" className="header">
         <h2>resizer</h2>
       </div>
-
       <div id="directions-content" className="content">
         <div className="content directions-content">
           <textarea id="directions-text" />
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 
-function Recipe(props) {
+function RecipeItem() {
   return (
-    <div id="recipe-content" className="content">
-      <div className="recipe-item ingredient">
-        <input type="text" name="ingredient-1" className="recipe-item ingredient-input" />
-      </div>
-      <div className="recipe-item amount">
-        <input type="text" name="amount-1" className="recipe-item amount-input" />
-      </div>
-      <div className="recipe-item unit">
-        <input type="text" name="unit-1" list="unit" className="recipe-item unit-input" />
-        <datalist id="unit">
-          <option value="grams" defaultValue>grams</option>
-          <option value="cups">cups</option>
-          <option value="tbsp">tbsp</option>
-          <option value="tsp">tsp</option>
-        </datalist>
-      </div>
+    <div className="recipe-item">
+      <input type="text" name="ingredient-1" className="recipe-item ingredient-input" />
+      <input type="text" name="amount-1" className="recipe-item amount-input" />
+      <input type="text" name="unit-1" list="unit" className="recipe-item unit-input" />
+      <datalist id="unit">
+        <option value="grams" defaultValue>grams</option>
+        <option value="cups">cups</option>
+        <option value="tbsp">tbsp</option>
+        <option value="tsp">tsp</option>
+      </datalist>
     </div>
   );
 }
 
-function RecipeItem(props) {
-
+function Recipe() {
+  return (
+    <div id="recipe-content" className="content">
+      <RecipeItem />
+    </div>
+  );
 }
 
 export default Recipe;
