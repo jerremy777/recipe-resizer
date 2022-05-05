3490b67aaa533a74aa6d5014f5b26c4650462f3e
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-04 18:27:33 -0700

    Creating MVC layout

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 const express = require('express');
+const db = require('./db');
 
 const app = express();
 const port = process.env.PORT;
