9f5ed3ad4f22608d54c48f9173d914627e85a580
Jerremy Stroman <jerremystroman@gmail.com>
2022-05-05 13:07:01 -0700

    Style updates!

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { render } from "react-dom";
-import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
-import "./styles.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+import './styles.css';
 
-
-const mountNode = document.getElementById("app");
+const mountNode = document.getElementById('app');
 const root = createRoot(mountNode);
 root.render(<App name="Jane" />);
